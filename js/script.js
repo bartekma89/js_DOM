c2e9b82fafc7a40a82f082b517d5b13020739f3e
Bartek <bartekma89@gmail.com>
2017-11-18 23:40:11 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var containerElements = document.getElementsByClassName('button');
+
+containerElementsLength = containerElements.length;
+
+for(var i = 0; i < containerElementsLength; i++) {
+
+	alert(containerElements[i].innerHTML);
+	
+}
